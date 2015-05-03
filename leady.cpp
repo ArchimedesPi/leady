02833b9403cafedefe5068d82d3e4f82dc85c0c1
Liam Marshall <archimedespi3141@gmail.com>
2015-05-03 16:49:30 -0500

    Implement all the things

--- leady.cpp
+++ leady.cpp
@@ -2,7 +2,6 @@
 
 
 namespace LEaDy {
-
 	LED::LED(int pin) {
 		this->pin = pin;
 	}
@@ -12,12 +11,33 @@ namespace LEaDy {
 		return LEaDy::cie[intensity];
 	}
 	void LED::setIntensity(float intensity) {
-		setRawPWM(correctIntensity(intensity));
+		setRawPWM(correctIntensity(int(intensity)));
 	}
 	void LED::off() {
 		setRawPWM(0);
 	}
 	void LED::setRawPWM(int pwm) {
 		analogWrite(this->pin, pwm);
+	}
+
+	RGBLED::RGBLED(rPin, gPin, bPin) {
+		this->r = LEaDy::LED(rPin);
+		this->g = LEaDy::LED(gPin);
+		this->b = LEaDy::LED(bPin);
+	}
+	void RGBLED::setRGB(float r, float g, float b) {
+		this->r::setIntensity(r);
+		this->g::setIntensity(g);
+		this->b::setIntensity(b);
+	}
+	void RGBLED::off() {
+		this->r::off();
+		this->g::off();
+		this->b::off();
+	}
+	void RGBLED::onWhite(float intensity) {
+		this->r::setIntensity(intensity);
+		this->g::setIntensity(intensity);
+		this->b::setIntensity(intensity);
 	}
 }
