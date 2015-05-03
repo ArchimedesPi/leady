70d7a5855add3303156bcf3c9fae76f32fe7166c
Liam M <ArchimedesPi@users.noreply.github.com>
2015-05-03 13:07:11 -0500

    Split and namespace (only in leady.h right now)

--- leady.h
+++ leady.h
@@ -3,22 +3,34 @@
 
 #include <Arduino.h>
 
-class LEaDy {
-public:
-    LEaDy();
-    void setRGB(float r, float g, float b);
-    void setHSV(float h, float s, float v);
-    void off();
-    void onWhite(float intensity);
-private:
-    // -- pin definitions
-    int rPin;
-    int gPin;
-    int bPin;
-    // -- pin statuses
-    int rPwm;
-    int gPwm;
-    int bPwm;
-    void setRawRGBPWM(int r, int g, int b);
+namespace LEaDy {
+    class LED {
+    public:
+        LED(pin);
+        void setIntensity(float intensity);
+        void off();
+    private:
+        int pin;
+        int pwm;
+        void setRawPWM(int p);
+    }
+    class RGBLED {
+    public:
+        RGBLED(rPin, gPin, bPin);
+        void setRGB(float r, float g, float b);
+        void setHSV(float h, float s, float v);
+        void off();
+        void onWhite(float intensity);
+    private:
+        // -- pin definitions
+        int rPin;
+        int gPin;
+        int bPin;
+        // -- pin statuses
+        int rPwm;
+        int gPwm;
+        int bPwm;
+        void setRawRGBPWM(int r, int g, int b);
+    }
 }
     
