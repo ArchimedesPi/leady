dd2812a2bc00ceb6bc7f77166c681196c3a27f36
Liam Marshall <archimedespi3141@gmail.com>
2015-05-03 16:41:23 -0500

    basic implementation

--- leady.cpp
+++ leady.cpp
@@ -1,4 +1,23 @@
 #include "leady.h"
 
 
+namespace LEaDy {
 
+	LED::LED(int pin) {
+		this->pin = pin;
+	}
+	// intensity ranges [0, 1024]
+	// returns [0, 255] corrected with CIE1931
+	int LED::correctIntensity(int intensity) {
+		return LEaDy::cie[intensity];
+	}
+	void LED::setIntensity(float intensity) {
+		setRawPWM(correctIntensity(intensity));
+	}
+	void LED::off() {
+		setRawPWM(0);
+	}
+	void LED::setRawPWM(int pwm) {
+		analogWrite(this->pin, pwm);
+	}
+}
